fbb9bb9cdff8b5e9e6e30e0bf72a7d40f8d7da4c
badgelatte <saucy5939@gmail.com>
2024-03-28 16:28:41 +0900

    feature: register getMapping 추가
    register 페이지로 바로 띄우는 method 추가, pom.xml java 11로 수정, MemberRequestDto @AllArgsConstructor로 수정

--- src/main/java/com/front/member/dto/MemberRequestDto.java
+++ src/main/java/com/front/member/dto/MemberRequestDto.java
@@ -1,9 +1,8 @@
 package com.front.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class MemberRequestDto {
     String id;
