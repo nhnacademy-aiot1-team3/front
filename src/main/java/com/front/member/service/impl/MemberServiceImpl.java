8f86a571037db36c3590df769ca3d2770e542ca3
badgelatte <saucy5939@gmail.com>
2024-04-01 18:04:57 +0900

    feat: javadoc 추가
    javadoc 추가

--- src/main/java/com/front/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/front/member/service/impl/MemberServiceImpl.java
@@ -14,11 +14,21 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-
+/**
+ * 사용자 관련 작업 수행하는 service
+ * @author 이지현
+ * @version 1.0
+ */
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
     private final MemberAdaptor memberAdaptor;
+
+    /**
+     * 로그인 요청 정보 바탕으로 인증 절차 진행, 인증 결과에 따른 토큰 정보 반환
+     * @param memberRequestDto 사용자 가입 정보 (id, pw)
+     * @return 로그인 성공시 토큰 정보를 Optional에 담아서 return, 로그인 실패시 빈 객체를 Optional에 담아서 return
+     */
     @Override
     public Optional<ResponseDto<ResponseHeaderDto,TokenResponseDto>> doLogin(MemberRequestDto memberRequestDto) {
         ResponseEntity<ResponseDto<ResponseHeaderDto, TokenResponseDto>> answer = memberAdaptor.doLogin(memberRequestDto);
@@ -27,6 +37,10 @@ public class MemberServiceImpl implements MemberService {
         return Optional.ofNullable(answer.getBody());
     }
 
+    /**
+     * 회원 가입 정보를 바탕으로 회원 가입 절차 진행
+     * @param memberRequestDto 사용자 가입 정보 (id, pw)
+     */
     @Override
     public void doRegister(MemberRequestDto memberRequestDto) {
         memberAdaptor.doRegister(memberRequestDto);
