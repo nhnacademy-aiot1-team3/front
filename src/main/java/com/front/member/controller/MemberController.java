0aef9f0359287e13bd4b676f05c7b24dc4e93dcf
chaehyonNa <skcogus12@naver.com>
2024-04-04 11:07:43 +0900

    Feat: profile 페이지
    profile 페이지 추가

--- src/main/java/com/front/member/controller/MemberController.java
+++ src/main/java/com/front/member/controller/MemberController.java
@@ -132,4 +132,8 @@ public class MemberController {
         return (String)request.getAttribute("path");
     }
 
+    @GetMapping("/profile")
+    public String getProfile(){
+        return "profile";
+    }
 }
