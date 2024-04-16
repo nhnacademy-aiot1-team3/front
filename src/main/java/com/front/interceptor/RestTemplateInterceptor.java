c8f101ea682e7311f98b0c82b2208399df3149c2
chaehyonNa <skcogus12@naver.com>
2024-04-16 16:08:35 +0900

    Feat: rest template interceptor 추가 아직 수정중
    RestTemplate interceptor 추가 아직 수정중

---
+++ src/main/java/com/front/interceptor/RestTemplateInterceptor.java
@@ -0,0 +1,18 @@
+package com.front.interceptor;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+public class RestTemplateInterceptor implements ClientHttpRequestInterceptor {
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+//        HttpHeaders httpHeaders = request.getHeaders();
+        ClientHttpResponse response = execution.execute(request, body);
+        response.getHeaders().add("Authorization", "Bearer " + response.getHeaders().getFirst("access_token"));
+        return response;
+    }
+}
