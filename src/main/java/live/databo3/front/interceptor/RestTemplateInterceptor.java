f3df28a2f741e8813bfab0e9849f8457e33e94af
badgelatte <saucy5939@gmail.com>
2024-04-26 11:32:54 +0900

    Feat: Interceptor 제외하도록 변경
    경로에 /duplication을 가지고 있는 controller는 제외하도록 작성

--- src/main/java/live/databo3/front/interceptor/RestTemplateInterceptor.java
+++ src/main/java/live/databo3/front/interceptor/RestTemplateInterceptor.java
@@ -21,10 +21,16 @@ public class RestTemplateInterceptor implements ClientHttpRequestInterceptor {
         this.httpServletRequest = httpServletRequest;
     }
 
+
+
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
         HttpHeaders headers = request.getHeaders();
 
+        log.debug(request.getURI().getPath());
+        if(request.getURI().getPath().contains("/duplicate")) {
+            return execution.execute(request, body);
+        }
         String accessToken = extractAccessTokenFromCookie(httpServletRequest);
 
         if (accessToken != null && !accessToken.isEmpty()) {
@@ -41,6 +47,7 @@ public class RestTemplateInterceptor implements ClientHttpRequestInterceptor {
         }
         return accessTokenCookie.getValue();
 //        Cookie[] cookies = request.getCookies();
+
 //        if (cookies != null) {
 //            for (Cookie cookie : cookies) {
 //                if ("access_token".equals(cookie.getName())) {
