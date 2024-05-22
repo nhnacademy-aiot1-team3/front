711169bb6c39b6a0ff58d888d646c4314c4b913e
badgelatte <saucy5939@gmail.com>
2024-05-22 18:03:33 +0900

    feat: notification adaptor 추가
    notification adaptor 추가

---
+++ src/main/java/live/databo3/front/adaptor/NotificationAdaptor.java
@@ -0,0 +1,20 @@
+package live.databo3.front.adaptor;
+
+import live.databo3.front.admin.dto.request.ModifyNotificationRequest;
+import live.databo3.front.dto.GetNotificationDto;
+import live.databo3.front.dto.GetNotificationListResponse;
+import live.databo3.front.admin.dto.NotificationDto;
+
+import java.util.List;
+
+public interface NotificationAdaptor {
+    List<GetNotificationListResponse> getAllNotifications();
+
+    GetNotificationDto getNotification(Long notificationId);
+
+    void postNotification(NotificationDto notification);
+
+    void putNotification(Long notificationId, ModifyNotificationRequest request);
+
+    void deleteNotification(Long notificationId);
+}
