341338a0f9638f06ae9c7d5ffa3ba35f7dcb3b3e
badgelatte <saucy5939@gmail.com>
2024-05-30 17:46:15 +0900

    feat: 배터리 잔량에 관한 정보를 가져오는 dto
    place, device, branch, value를 가져옴

---
+++ src/main/java/live/databo3/front/owner/dto/BatteryLevelDto.java
@@ -0,0 +1,11 @@
+package live.databo3.front.owner.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BatteryLevelDto {
+    private String place;
+    private String device;
+    private String branch;
+    private int value;
+}
